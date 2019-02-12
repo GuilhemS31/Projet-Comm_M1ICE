e2b466197b08bdd4c4d2ccad959b1f5c06be7d05
Arnaud.S <sibade.arnaud@gmail.com>
2019-02-12 16:57:26 +0100

    feature : imageBack, Driver Cam

--- src/js/struct.case.js
+++ src/js/struct.case.js
@@ -1,30 +1,47 @@
 function Case() {
-    this.gauche = null
-    this.droite = null
-    this.haut = null
-    this.bas = null
+  this.gauche = null
+  this.droite = null
+  this.haut = null
+  this.bas = null
 
-    this.font = null
-    this.document = null
+  this.font = null
+  this.document = null
+}
+
+function Case(gauche, droite, haut, bas, font) {
+  this.droite = droite
+  this.gauche = gauche
+  this.haut = haut
+  this.bas = bas
+
+  this.font = font
+  this.document = null
+}
+
+function Case(gauche, droite, haut, bas, font, document) {
+  this.droite = droite
+  this.gauche = gauche
+  this.haut = haut
+  this.bas = bas
+
+  this.font = font
+  this.document = document
+}
+
+getFont = function() {
+  //TODO
+  if (this.font === "mer"){
+    return cheminMer.png 
+  } else if (this.font === "neige"){
+    return cheminMer.png 
+  } else if (this.font === "ville"){
+    return cheminMer.png 
+  } else if (this.font === "camp"){
+    return cheminMer.png 
+  } else if (this.font === "foret"){
+    return cheminMer.png 
+  } else {
+    console.warn("Erreur lors de la recherche de l'image de font qui ne correspond a aucune des images possible")
+    return ""
   }
-
-  function Case(gauche, droite, haut, bas, font) {
-    this.droite = droite
-    this.gauche = gauche
-    this.haut = haut
-    this.bas = bas
-
-    this.font = font
-    this.document = null
-  }
-
-  function Case(gauche, droite, haut, bas, font, document) {
-    this.droite = droite
-    this.gauche = gauche
-    this.haut = haut
-    this.bas = bas
-
-    this.font = font
-    this.document = document
-  }
-
+}
--- src/js/struct.element.js
+++ src/js/struct.element.js
@@ -1,4 +1,15 @@
 function Elem(){
     this.decouvert = false
     this.name = null
+    this.chemin = null
+}
+
+function Elem(name, chemin){
+    this.decouvert = false
+    this.name = name
+    this.chemin = chemin
+}
+
+function decouverte(){
+    this.decouvert=true
 }
