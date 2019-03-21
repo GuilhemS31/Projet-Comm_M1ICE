0521783c5cf893b1c734bc42698b7dd6fa10b5f7
Unknown <guilhem31.susa@hotmail.com>
2019-03-21 21:55:02 +0100

    Commencer mise en place bouton launchGame, remove map , remove méthodes dand fileHelper.js

--- src/js/struct.case.js
+++ src/js/struct.case.js
@@ -4,44 +4,26 @@ function Case() {
   this.haut = null
   this.bas = null
 
-  this.font = null
+  this.fond = null
   this.document = null
 }
 
-function Case(gauche, droite, haut, bas, font) {
+function Case(gauche, droite, haut, bas, fond) {
   this.droite = droite
   this.gauche = gauche
   this.haut = haut
   this.bas = bas
 
-  this.font = font
+  this.fond = fond
   this.document = null
 }
 
-function Case(gauche, droite, haut, bas, font, document) {
+function Case(gauche, droite, haut, bas, fond, document) {
   this.droite = droite
   this.gauche = gauche
   this.haut = haut
   this.bas = bas
 
-  this.font = font
+  this.fond = fond
   this.document = document
-}
-
-getFont = function() {
-  //TODO
-  if (this.font === "mer"){
-    return cheminMer.png 
-  } else if (this.font === "neige"){
-    return cheminMer.png 
-  } else if (this.font === "ville"){
-    return cheminMer.png 
-  } else if (this.font === "camp"){
-    return cheminMer.png 
-  } else if (this.font === "foret"){
-    return cheminMer.png 
-  } else {
-    console.warn("Erreur lors de la recherche de l'image de font qui ne correspond a aucune des images possible")
-    return ""
-  }
 }
--- src/js/struct.pers.js
+++ src/js/struct.pers.js
@@ -9,12 +9,3 @@ function Perso(prenom, nom, lInfos){
     this.Nom = nom
     this.infos = lInfos
 }
-
-function getInfos(name) {
-    const info = this.infos.forEach(elem => { if (elem.name === name) { return elem } });
-    if (info != null){
-        return info
-    } else {
-        console.warn("Attention aucun element trouver pour le name : ",name)
-    }
-}
