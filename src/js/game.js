ac6941c09998e3be428313380c61d0bf05b64a29
Unknown <guilhem31.susa@hotmail.com>
2019-03-21 22:39:35 +0100

    Avancéee

---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+function launchGame(){
+  //remove btnPlay
+
+  //Séparer zone de jeu en 2 :
+  //Gauche = Map
+  initMap()
+  //Droite = jeu
+  initGame()
+}
+
+
+function initMap(){
+
+}
+
+function initGame(){
+
+}
